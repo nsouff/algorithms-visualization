023ffdabc1b9cd59bd1926919d2b966f07347ba8
Nathan Souffan <souffan.nathan@gmail.com>
2020-09-02 12:07:05 +0200

    Add the backtracking algo and fix bugs

--- backtracking.py
+++ backtracking.py
@@ -3,22 +3,25 @@ import sys
 
 black = (0, 0, 0)
 white = (255,255,255)
+clock = None
+digits = [None for _ in range(9)]
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
 else:
-    filename = 'sudoku' # default sudoku grid
+    filename = 'sudoku' # default sudoku grid file
 
 
-def grid(filename):
+def init_grid(filename):
     grid = [[0 for _ in range(9)] for _ in range(9)]
     with open(filename) as file:
         for i in range(9):
             for j in range(9):
                 grid[i][j] = int(file.read(1))
             assert(file.read(1) == '\n')
-
+    return grid
 def draw(grid, window):
+    window.fill(white)
     for i in range(9):
         width = 1
         if i%3 == 0:
@@ -26,9 +29,13 @@ def draw(grid, window):
         pygame.draw.line(window, black, (i*100, 0), (i*100, 900), width)
         pygame.draw.line(window, black, (0, i*100), (900, i*100), width)
         for j in range(9):
-            if gird[i][j] != 0:
-                pass
-
+            if grid[i][j] != 0:
+                for n in range(1, 10):
+                    if grid[i][j] == n:
+                        window.blit(digits[n-1], (j*100 + 30, i*100 + 30))
+                        pass
+    clock.tick(10)
+    pygame.display.update()
 def valid_number(grid, n, x, y):
     for i in range(9):
         if grid[x][i] == n or grid[i][y] == n:
@@ -41,16 +48,43 @@ def valid_number(grid, n, x, y):
 
     return True
 
+def find_pos(grid):
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] == 0:
+                return (i, j)
+    return False
+
+def solve(grid, window):
+    draw(grid, window)
+    pos = find_pos(grid)
+    if not pos:
+        return True
+    x, y = pos
+    for n in range(1, 10):
+        if valid_number(grid, n, x, y):
+            grid[x][y] = n
+            if solve(grid, window):
+                return True
+            grid[x][y] = 0
+    return False
+
+def init_digits():
+    global digits
+    font = pygame.font.SysFont("Arial", 50)
+    for i in range(9):
+        digits[i] = font.render(str(i+1), True, black)
 
 
 def main():
     pygame.init()
     window = pygame.display.set_mode((900, 900))
-    s = Sudoku('sudoku')
-    window.fill(white)
-    s.draw(window)
-    pygame.display.update()
-    while True:
-        pass
+    pygame.display.set_caption("Sudoku Solver")
+    global clock
+    clock = pygame.time.Clock()
+    grid = init_grid(filename)
+    init_digits()
+    draw(grid, window)
+    solve(grid, window)
 if __name__ == '__main__':
     main()
